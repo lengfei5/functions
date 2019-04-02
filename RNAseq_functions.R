73e855066ad9ed31007c95f4a3db2efa74a4b59e
jingkui.wang <jingkui.wang@imp.ac.at>
2019-04-02 11:48:11 +0200

    clean RNAseq_functions.R

--- RNAseq_functions.R
+++ RNAseq_functions.R
@@ -1,7 +1,7 @@
 ############# 
 ##### Collection of functions for RNA-seq data
 ############# 
-Check.RNAseq.Quality = function(read.count, design.matrix)
+Check.RNAseq.Quality = function(read.count, design.matrix, keep.All = FALSE, norms=NULL, Threshold.read.counts=10)
 {
   require(lattice);
   require(ggplot2)
@@ -9,7 +9,7 @@ Check.RNAseq.Quality = function(read.count, design.matrix)
   library("vsn");
   library("pheatmap");
   library("RColorBrewer");
-  library("dplyr");  
+  library("dplyr"); 
   library("ggplot2")
   #load(file=paste0('Rdata/Screen_countData_sgRNA_Gene_clean_mapping', version.data, '.Rdata'))
   # kk = grep('Ab', colnames(bb))
@@ -23,18 +23,20 @@ Check.RNAseq.Quality = function(read.count, design.matrix)
   dim(raw)
   raw[which(is.na(raw))] = 0
   xx = raw;
-  par(cex = 1.8, las = 1, mgp = c(1.6,0.5,0), mar = c(6,10,2,0.8)+0.1, tcl = -0.3)
+  
+  par(cex = 1.8, las = 1, mgp = c(1.6,0.5,0), mar = c(6,16,2,0.8)+0.1, tcl = -0.3)
   par(mfrow=c(1,1))
   
   total = apply(raw, 2, sum)
-  
   cc.uniq = unique(cc);
   cols = match(cc, cc.uniq)
   #cols = (c(1:length(unique(cc)))-1)%/%3+1
   barplot(total/10^6, horiz = TRUE, names.arg = colnames(raw), las=1, col = cols, main='Total nb of reads quantified for features', xlab='number of reads (Million)')
-  abline(v=seq(0, 20, by=5), col='red', lty=1, lwd=2.0);#abline(v=c(20, 45), col='red', lty=1, lwd=2.0)
+  abline(v=c(1, 2, seq(5, 20, by=5)), col='red', lty=1, lwd=2.0);#abline(v=c(20, 45), col='red', lty=1, lwd=2.0)
   xx = raw
   xx[which(xx==0)] = NA
+  
+  par(cex = 1.0, las = 1, mgp = c(2,0.2,0), mar = c(16,3,2,0.2), tcl = -0.3)
   boxplot(log10(xx), las=3, col=cols, ylab='log10(nb of reads)', main='Read distribution for features')
   
   ### make DESeq object using read counts and design matrix
@@ -42,15 +44,24 @@ Check.RNAseq.Quality = function(read.count, design.matrix)
   conds = factor(paste0(colnames(design.matrix)[-1], collapse = " + "))
   eval(parse(text = paste0("dds <- DESeqDataSetFromMatrix(countData, DataFrame(design.matrix), design = ~ ", conds, ")")))
   #dds <- DESeqDataSetFromMatrix(countData, DataFrame(design.matrix), design = ~ condition + time)
-  dds <- dds[ rowSums(counts(dds)) > 10, ]
-  dds <- estimateSizeFactors(dds)
+  
+  if(!keep.All){
+    dds <- dds[ rowSums(counts(dds)) > Threshold.read.counts, ]
+  }
+  
+  if(is.null(norms)){
+    dds <- estimateSizeFactors(dds)
+  }else{
+    sizeFactors(dds) = norms;
+  }
+  
   fpm = fpm(dds, robust = TRUE)
-  vsd <- vst(dds, blind = FALSE)
+  vsd <- varianceStabilizingTransformation(dds, blind = FALSE)
   
   ### boxplot (distributions of fpm) for all samples
   xx = as.matrix(fpm)
   par(mfrow=c(1,1))
-  par(cex = 1.0, las = 1, mgp = c(2,0.2,0), mar = c(10,3,2,0.2), tcl = -0.3)
+  par(cex = 1.0, las = 1, mgp = c(2,0.2,0), mar = c(16,3,2,0.2), tcl = -0.3)
   for(n in 1:ncol(xx)){
     kk = which(xx[,n]>0);
     if(n==1) boxplot(log2(xx[kk, n]), horizontal = FALSE, las=2, at=(n), ylim=c(-6, 23), xlim=c(0, (ncol(xx)+1)), names=as.character(colnames(xx)[n]),
@@ -69,12 +80,6 @@ Check.RNAseq.Quality = function(read.count, design.matrix)
   M <- cor(xx, use = "na.or.complete")
   #corrplot(M, method="circle", type = 'upper', order="hclust")
   corrplot(M, method="ellipse", order="hclust", tl.cex=1.2, cl.cex=0.7, tl.col="black", addrect=ceiling(ncol(xx)/2), col=col1(100), rect.col=c('green'), rect.lwd=2.0)
-  
-  ###  pairwise correlation and fitting (to chek if there is batch effect)
-  yy = as.matrix(fpm)
-  yy[which(yy==0)] = NA;
-  yy = log2(yy)
-  pairs(yy, lower.panel=NULL, upper.panel=panel.fitting)
   
   ### count transformation using vsd
   library("dplyr")
@@ -117,9 +122,33 @@ Check.RNAseq.Quality = function(read.count, design.matrix)
            col = colors)
   
   ### clustering samples using PCA analysis
-  pca=plotPCA(vsd, intgroup = colnames(design.matrix)[-1])
+  pca=plotPCA(vsd, intgroup = colnames(design.matrix)[-1], returnData = FALSE)
   print(pca)
   
+  Show.sample.names.PCA.Clusters = FALSE 
+  if(Show.sample.names.PCA.Clusters){
+    pca2save = as.data.frame(plotPCA(vsd, intgroup = colnames(design.matrix)[-1], returnData = TRUE))
+    #p = ggplot(data=pca2save, aes(PC1, PC2, label = name, color=condition, shape=time)) + geom_point(size=3)
+    #p + geom_text(hjust = 0.5, nudge_y = 0.1, size=2.5) 
+    ggp = ggplot(data=pca2save, aes(PC1, PC2, label = name, color=condition, shape=time)) + geom_point(size=3) +
+      geom_text(hjust = 0.7, nudge_y = 1, size=2.5)  
+    plot(ggp);
+  }
+  
+  ###  pairwise correlation and fitting (to chek if there is batch effect)
+  if(ncol(fpm)<20)
+  {
+    yy = as.matrix(fpm)
+    yy[which(yy==0)] = NA;
+    yy = log2(yy)
+    pairs(yy, lower.panel=NULL, upper.panel=panel.fitting)
+  }
+  
+  #ggplot(pcaData, aes(PC1, PC2, color=condition, shape=type)) +
+  #  geom_point(size=3) +
+  #  xlab(paste0("PC1: ",percentVar[1],"% variance")) +
+  #  ylab(paste0("PC2: ",percentVar[2],"% variance")) + 
+  #  coord_fixed()
   #write.table(processed, file=paste0('Table_quanseq_WT_tbx_negative_positive', version.analysis, '.txt'), sep='\t', quote=FALSE, col.names = TRUE, row.names = FALSE)
   #kk = grep('fpm', colnames(process
 }
@@ -142,7 +171,7 @@ panel.cor <- function(x, y, digits=2, prefix="", cex.cor)
   text(.8, .8, Signif, cex=cex, col=2) 
 }
 
-panel.fitting = function (x, y, bg = NA, pch = par("pch"), cex = 0.15, col='black') 
+panel.fitting = function (x, y, bg = NA, pch = par("pch"), cex = 0.5, col='black') 
 {
   #x = yy[,1];y=yy[,2];
   #kk = which(x>0 & y>0); x=x[kk];y=y[kk]
