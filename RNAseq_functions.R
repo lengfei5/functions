3d0d5dcff1ba6e596c1038441a889a45ef63a354
jingkui.wang <jingkui.wang@imp.ac.at>
2020-03-18 13:15:50 +0100

    update RNAseq_functions.R

--- RNAseq_functions.R
+++ RNAseq_functions.R
@@ -4,29 +4,71 @@
 cat.countTable = function(xlist, countsfrom = 'featureCounts')
 {
   ## input is list.files for count tables (including directories and file names)
-  counts = NULL
-  for(n in 1:length(xlist)){
-    # n = 1
-    cat(xlist[n], '--\n')
-    if(countsfrom == 'featureCounts') {
+  if(countsfrom == "featureCounts"){
+    counts = NULL
+    for(n in 1:length(xlist)){
+      # n = 1
+      cat(xlist[n], '--\n')
+      
       ff = read.delim(xlist[n], sep='\t', header = TRUE, as.is = c(1), comment.char = "#");
       ff = ff[, c(1, ncol(ff))]
-    }else{
-      cat('no code for this option \n')
+      
+      if(n==1){
+        ggs = unique(ff[, 1]);
+        counts = data.frame(ggs, ff[match(ggs, ff[, 1]) , -1], stringsAsFactors = FALSE);
+      }else{
+        ggs = unique(c(counts[, 1], ff[, 1]));
+        counts = data.frame(ggs, counts[match(ggs, counts[, 1]), -1], ff[match(ggs, ff[, 1]) , -1], stringsAsFactors = FALSE);
+      }
     }
     
-    if(n==1){
-      ggs = unique(ff[, 1]);
-      counts = data.frame(ggs, ff[match(ggs, ff[, 1]) , -1], stringsAsFactors = FALSE);
-    }else{
-      ggs = unique(c(counts[, 1], ff[, 1]));
-      counts = data.frame(ggs, counts[match(ggs, counts[, 1]), -1], ff[match(ggs, ff[, 1]) , -1], stringsAsFactors = FALSE);
+  }else{
+    counts = NULL
+    if(countsfrom == "htseq"){
+      for(n in 1:length(xlist))
+      {
+        # n = 1
+        cat(n, '\t')
+        cat(xlist[n], '\n')
+        
+        if(n==1){
+          counts = read.table(xlist[n], header = FALSE);
+          colnames(counts) = c('RefseqID', basename(xlist[n]))
+          counts = data.frame(counts, stringsAsFactors = FALSE)
+        }else{
+          test = read.table(xlist[n], header = FALSE);
+          colnames(test) = c('RefseqID',  basename(xlist[n]))
+          mm = match(counts$RefseqID, test$RefseqID)
+          counts = data.frame(counts, test[mm, -grep('RefseqID', colnames(test))])
+        }
+      }
+    }
+    
+    if(countsfrom == 'slamdunk'){
+      for(n in 1:length(xlist))
+      {
+        # n = 1
+        cat(n, '\t')
+        cat(xlist[n], '\n')
+        
+        if(n==1){
+          counts = read.table(xlist[n], header = TRUE);
+          counts = data.frame(counts$Name, counts$ReadCount, stringsAsFactors = FALSE)
+          colnames(counts) = c('RefseqID', basename(xlist[n]))
+          #counts = data.frame(counts, stringsAsFactors = FALSE)
+        }else{
+          test = read.table(xlist[n], header = TRUE);
+          test = data.frame(test$Name, test$ReadCount, stringsAsFactors = FALSE)
+          colnames(test) = c('RefseqID',  basename(xlist[n]))
+          #mm = match(counts$RefseqID, test$RefseqID)
+          counts = data.frame(counts, test[, 2])
+        }
+      }
     }
   }
   
   colnames(counts)[1] = 'gene'
   colnames(counts)[-1] = basename(xlist)
-  
   return(counts)
   
 }
@@ -43,9 +85,10 @@ merge.countTables.htseq = function(file_list){
 }
 
 
-process.countTable = function(all, design, special.column = NULL, ensToGeneSymbol = TRUE, mergeCounts.forSameGeneSysbols = TRUE)
+process.countTable = function(all, design, special.column = NULL, ensToGeneSymbol = FALSE, mergeCounts.forSameGeneSysbols = TRUE)
 {
   index = c()
+  index.design.with.found.sample = c()
   for(n in 1:nrow(design))
   {
     #n = 1;
@@ -54,27 +97,38 @@ process.countTable = function(all, design, special.column = NULL, ensToGeneSymbo
    
     if(length(jj)==1) {
       index = c(index,jj)
-    }else{print(paste0("ERROR for sample--", design$SampleID[n]))}
+      index.design.with.found.sample = c(index.design.with.found.sample, n)
+    }else{
+      print(paste0("ERROR for sample--", design$SampleID[n]))
+    }
   }
   
-  newall = data.frame(as.character(all[,1]),  as.matrix(all[, index]), stringsAsFactors = FALSE)
+  newall = data.frame(as.character(all[, 1]),  as.matrix(all[, index]), stringsAsFactors = FALSE)
   colnames(newall)[1] = "gene"
   kk = which(colnames(design)=='SampleID')
   kk = c(setdiff(c(1:ncol(design)), kk), kk)
-  colnames(newall)[-1] = apply(design[, kk], 1, function(x)  paste(x, collapse = "_"))
+  colnames(newall)[-1] = apply(design[, kk], 1, function(x)  paste(x, collapse = "_"))[index.design.with.found.sample]
+  colnames(newall) = sapply(colnames(newall), function(x) gsub(' ', '', x))
   #paste(design$gen, "_", design$tissue.cell, "_", design$SampleID)
   
   if(ensToGeneSymbol){
-    options(warn=2)
-    result <- try(load(file='/Volumes/groups/cochella/jiwang/annotations/BioMart_WBcel235.Rdata'), silent=TRUE)
-    if (class(result) == "try-error"){
-      load(file='/Volumes/groups-1/cochella/jiwang/annotations/BioMart_WBcel235.Rdata')
-    }else{
-      load(file='/Volumes/groups/cochella/jiwang/annotations/BioMart_WBcel235.Rdata')
-    }
-    options(warn = -1)
-    mm = match(newall$gene, annot$WormBase.Gene.ID)
-    newall$gene = as.character(annot$Gene.name[mm])
+    annot = read.csv(file = "/Volumes/groups/cochella/jiwang//annotations/BioMart_WBcel235_noFilters.csv", header = TRUE, stringsAsFactors = FALSE)
+    #annot = data.frame(annot, stringsAsFactors = FALSE)
+    #######
+    # the following is only for protein-coding genes
+    #######
+    # options(warn=2)
+    # result <- try(load(file='/Volumes/groups/cochella/jiwang/annotations/BioMart_WBcel235.Rdata'), silent=TRUE)
+    # if (class(result) == "try-error"){
+    #   load(file='/Volumes/groups-1/cochella/jiwang/annotations/BioMart_WBcel235.Rdata')
+    # }else{
+    #   load(file='/Volumes/groups/cochella/jiwang/annotations/BioMart_WBcel235.Rdata')
+    # }
+    # options(warn = -1)
+    
+    mm = match(newall$gene, as.character(annot$Gene.stable.ID))
+    jj = which(!is.na(mm))
+    newall$gene[jj] = as.character(annot$Gene.name[mm[jj]])
     
     ss = apply(as.matrix(newall[, -1]), 1, sum)
     newall = newall[which(ss>0), ]
@@ -148,7 +202,31 @@ compare.readCount.UMI =function(design, aa, normalized = FALSE){
       
       # plot(apply(log2(umi[, jj]), 1, mean), vars.umi/vars.read, log='y')
     }
-    
   }
 }
+
+Merge.techinical.replicates.using.design.countTable = function(design, all, rep.technical=list(c("57751", "57753"), c("57752", "57754")))
+{
+  for(n in 1:length(rep.technical))
+  {
+    index = c()
+    for(id in rep.technical[[n]])
+    {
+      #print(id)
+      index = c(index, which(design$SampleID==id))
+    }
+    
+    design$SampleID[index[1]] = paste0(design$SampleID[index], collapse = ".")
+    ss = apply(all[, (index+1)], 1, function(x) sum(x, na.rm = TRUE))
+    all[, (index[1]+1)] = ss;
+    colnames(all)[(index[1]+1)] = paste0(design$genotype[index[1]], "_", design$tissue.cell[index[1]], "_", design$treatment[index[1]], "_",  design$SampleID[index[1]])
+    design = design[-index[-1], ]
+    all = all[, -(index[-1]+1)]
+  }
+  
+  return(list(design = design, 
+              countTable = all))
+}
+
+
 
